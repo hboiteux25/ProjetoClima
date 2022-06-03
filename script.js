9f43a9530961fa0af954f1dc067dccbbb98a4dd4
Henrique Boiteux <92280899+hboiteux25@users.noreply.github.com>
2022-06-03 19:14:28 -0300

    Conexão com API

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
+document.querySelector('.busca').addEventListener('submit', async(event)=>{
     event.preventDefault();
 
-    // PEGA A INFORMAÇÃO DIGITADA PELO USUÁRIO.
+// PEGA A INFORMAÇÃO DIGITADA PELO USUÁRIO.
     let input = document.querySelector('#searchInput').value; 
     
     if(input !== ''){
         showWarning('Carregando...')
-    }else{
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=c3c5ed5bdc12ead9ea8fcb602a2eba68&units=metric&lang=pt_br`
+
+        let results = await fetch(url);
+        let json = await results.json();
+
+        console.log(json)
+
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+
+            })
+        }else{
+            clearInfo()
+            showWarning('Não encontramos está localização')
 
     }
-  
+    }else{
+        clearInfo()
+    }
 });
 
+function clearInfo(){
+    showWarning('')
+    document.querySelector('.resultado').style.display = 'none';
+}
+
+function showInfo(json){
+    showWarning('');
+
+    document.querySelector('.resultado').style.display = 'block';
+    document.querySelector('.titulo').innerHTML=`${json.name}, ${json.country}`
+    document.querySelector('.tempInfo').innerHTML=`${json.temp} <sup>ºC</sup>`
+    document.querySelector('.ventoInfo').innerHTML=`${json.windSpeed} <span>km/h</span>`
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle}deg)`
+
+}
 
 //EM OUTSYSTEMS ISSO SERIA UM PARÂMETRO DE ENTRADA. 
 function showWarning(msg){
